f924a62dd5d306f7fbff7625a27d28756a128ac5
Ahmed <eng.ahmed.samirzakaria@gmail.com>
2022-07-29 00:54:19 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,267 @@
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  * @flow strict-local
+//  */
+
+// import React from 'react';
+// import type {Node} from 'react';
+// import {
+//   SafeAreaView,
+//   ScrollView,
+//   StatusBar,
+//   StyleSheet,
+//   Text,
+//   useColorScheme,
+//   View,
+// } from 'react-native';
+
+// import {
+//   Colors,
+//   DebugInstructions,
+//   Header,
+//   LearnMoreLinks,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
+
+// const Section = ({children, title}): Node => {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       <Text
+//         style={[
+//           styles.sectionTitle,
+//           {
+//             color: isDarkMode ? Colors.white : Colors.black,
+//           },
+//         ]}>
+//         {title}
+//       </Text>
+//       <Text
+//         style={[
+//           styles.sectionDescription,
+//           {
+//             color: isDarkMode ? Colors.light : Colors.dark,
+//           },
+//         ]}>
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// };
+
+// const App: () => Node = () => {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+//   const backgroundStyle = {
+//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+//   };
+
+//   return (
+//     <SafeAreaView style={backgroundStyle}>
+//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+//       <ScrollView
+//         contentInsetAdjustmentBehavior="automatic"
+//         style={backgroundStyle}>
+//         <Header />
+//         <View
+//           style={{
+//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
+//           }}>
+//           <Section title="Step One">
+//             Edit <Text style={styles.highlight}>App.js</Text> to change this
+//             screen and then come back to see your edits.
+//           </Section>
+//           <Section title="See Your Changes">
+//             <ReloadInstructions />
+//           </Section>
+//           <Section title="Debug">
+//             <DebugInstructions />
+//           </Section>
+//           <Section title="Learn More">
+//             Read the docs to discover what to do next:
+//           </Section>
+//           <LearnMoreLinks />
+//         </View>
+//       </ScrollView>
+//     </SafeAreaView>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+// });
+
+// export default App;
+
+import React, {useEffect, useRef, useState} from 'react';
+import {Platform, ScrollView, Text, TouchableOpacity, View} from 'react-native';
+import RtcEngine, {
+  RtcLocalView,
+  RtcRemoteView,
+  VideoRenderMode,
+} from 'react-native-agora';
+
+import requestCameraAndAudioPermission from './components/Permission';
+import styles from './components/Style';
+
+const config = {
+  appId: '8c5085e6ffa64e7a95ce62270d81a687',
+  token: null, //'YourTokenOrNull',
+  channelName: 'test',
+};
+
+const App = () => {
+  const _engine = useRef(null);
+  const [isJoined, setJoined] = useState(false);
+  const [peerIds, setPeerIds] = useState([]);
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      // Request required permissions from Android
+      requestCameraAndAudioPermission().then(() => {
+        console.log('requested!');
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    /**
+     * @name init
+     * @description Function to initialize the Rtc Engine, attach event listeners and actions
+     */
+    const init = async () => {
+      const {appId} = config;
+      _engine.current = await RtcEngine.create(appId);
+      await _engine.current.enableVideo();
+
+      _engine.current.addListener('Warning', warn => {
+        console.log('Warning', warn);
+      });
+
+      _engine.current.addListener('Error', err => {
+        console.log('Error', err);
+      });
+
+      _engine.current.addListener('UserJoined', (uid, elapsed) => {
+        console.log('UserJoined', uid, elapsed);
+        // If new user
+        if (peerIds.indexOf(uid) === -1) {
+          // Add peer ID to state array
+          setPeerIds(prev => [...prev, uid]);
+        }
+      });
+
+      _engine.current.addListener('UserOffline', (uid, reason) => {
+        console.log('UserOffline', uid, reason);
+        // Remove peer ID from state array
+        setPeerIds(prev => prev.filter(id => id !== uid));
+      });
+
+      // If Local user joins RTC channel
+      _engine.current.addListener(
+        'JoinChannelSuccess',
+        (channel, uid, elapsed) => {
+          console.log('JoinChannelSuccess', channel, uid, elapsed);
+          // Set state variable to true
+          setJoined(true);
+        },
+      );
+    };
+    init();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  /**
+   * @name startCall
+   * @description Function to start the call
+   */
+  const startCall = async () => {
+    // Join Channel using null token and channel name
+    await _engine.current?.joinChannel(
+      config.token,
+      config.channelName,
+      null,
+      0,
+    );
+  };
+
+  /**
+   * @name endCall
+   * @description Function to end the call
+   */
+  const endCall = async () => {
+    await _engine.current?.leaveChannel();
+    setPeerIds([]);
+    setJoined(false);
+  };
+
+  const _renderVideos = () => {
+    return isJoined ? (
+      <View style={styles.fullView}>
+        <RtcLocalView.SurfaceView
+          style={styles.max}
+          channelId={config.channelName}
+          renderMode={VideoRenderMode.Hidden}
+        />
+        {_renderRemoteVideos()}
+      </View>
+    ) : null;
+  };
+
+  const _renderRemoteVideos = () => {
+    return (
+      <ScrollView
+        style={styles.remoteContainer}
+        contentContainerStyle={styles.padding}
+        horizontal={true}>
+        {peerIds.map(value => {
+          return (
+            <RtcRemoteView.SurfaceView
+              style={styles.remote}
+              uid={value}
+              channelId={config.channelName}
+              renderMode={VideoRenderMode.Hidden}
+              zOrderMediaOverlay={true}
+            />
+          );
+        })}
+      </ScrollView>
+    );
+  };
+
+  return (
+    <View style={styles.max}>
+      <View style={styles.max}>
+        <View style={styles.buttonHolder}>
+          <TouchableOpacity onPress={startCall} style={styles.button}>
+            <Text style={styles.buttonText}> Start Call </Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={endCall} style={styles.button}>
+            <Text style={styles.buttonText}> End Call </Text>
+          </TouchableOpacity>
+        </View>
+        {_renderVideos()}
+      </View>
+    </View>
+  );
+};
+
+export default App;
---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  videoCall
+//
+//  Created by Ahmed Samir on 28/07/2022.
+//
+
+import Foundation
